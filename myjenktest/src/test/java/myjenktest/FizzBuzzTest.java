f65a445d5a35fc2dbc4af686829be6b5f2be6ef1
ValentinaAltamura95 <117814728+ValentinaAltamura95@users.noreply.github.com>
2022-11-09 16:47:32 +0100

    MyFirstTest
    TestPlayFunction

---
+++ myjenktest/src/test/java/myjenktest/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package myjenktest;
+
+import org.junit.jupiter.api.*;
+
+
+import myjenktest.tutorial.FizzBuzz;
+
+public class FizzBuzzTest {
+       public FizzBuzz fb;
+       
+       @BeforeEach
+       public void setup() {
+    	   fb=new FizzBuzz();
+       }
+       
+       @DisplayName("Play FizzBuzz with number=1")
+       @Test
+       public void testNumber () {
+    	   String risultato=fb.play(1);
+    	   //Verifica risultato ottenuto, risultato pianificato - True o False
+    	   Assertions.assertEquals(risultato, "1");
+       }
+}
